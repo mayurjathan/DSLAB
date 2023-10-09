1f907fc08db04a8bf32a16e8a7d770312c9a4f3c
mayurjathan <mayurjathan999@gmail.com>
2023-10-09 22:21:43 +0530

    changes

--- lab 1/selection sort.c
+++ lab 1/selection sort.c
@@ -9,19 +9,20 @@ int main()
   for (c = 0; c < n; c++)
     scanf("%d", &a[c]);
   for(i=0;i<n-1;i++)
+  {
+  s=i;
   for(c=i+1;c<n;c++)
   {
-      s=i;
+      
       if(a[s]>a[c])
-      {
           s=c;
+  }
           if(s!=i)
           {
           temp=a[i];
           a[i]=a[s];
           a[s]=temp;
       }
-  }
   }
    printf("sorted array : ");
  for (c = 0; c < n; c++)
